8f0b226a4eb9fdea0d181396589e2fdc6b85aaf8
Taavi Tilk <taavi.tilk@gmail.com>
2016-06-12 16:41:45 +0300

    parandatud

--- eksam/eksam.php
+++ eksam/eksam.php
@@ -12,7 +12,7 @@ session_start();
 
 $dbname = "test";
 $ip = getenv("REMOTE_ADDR");
-$count1 = 0;
+
 
 $cn = mysqli_connect("localhost", "test", "t3st3r123") or die("Ei saanud serveriga ühendust");
 //$cn = mysqli_connect("localhost", "root", "Password") or die("Ei saanud serveriga ühendust");
@@ -46,7 +46,7 @@ $tulemus = mysqli_fetch_assoc($result);
 $tulemus2 =implode($tulemus);
 $_SESSION["esimene"] =  $tulemus2;
 
-echo "KÜLASTANUD ". $_SESSION["esimene"]. " korda. <br/>";
+echo "KÜLASTANUD ". $_SESSION["esimene"]. " korda, IP aadresseil ".$ip."<br/>";
 
 
 } else{
@@ -60,7 +60,7 @@ $hits = "SELECT hits FROM ttilk__IP WHERE IP_aadress=\"$ip\"";
 $result = mysqli_query($cn, $hits) or die("Couldn't update hits");
 $tulemus = mysqli_fetch_assoc($result);
 $tulemus2 =implode($tulemus);
-echo "KÜLASTANUD ". $tulemus2. " korda. <br/>";
+echo "KÜLASTANUD ". $tulemus2. " korda, IP aadresseil ".$ip."<br/>";
 //echo "olete sellelt Ip aadressilt ". $ip . " juba meid külastanud ". $tulemus2. " korda.";
 }
 ?>
